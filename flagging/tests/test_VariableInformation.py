f4316cbd85b6951d749dd08cc491c8a234cd2287
Adam Kinnear <akinnear@gmail.com>
2020-06-05 13:19:13 -0700

    #8: Added an equality test

--- flagging/tests/test_VariableInformation.py
+++ flagging/tests/test_VariableInformation.py
@@ -2,7 +2,6 @@ from flagging.src.VariableInformation import VariableInformation
 
 
 def test_Create_complex_VariableInformation_str():
-    """Test making the variable a.b.c"""
     a = VariableInformation("a")
     b = VariableInformation("b", a)
     c = VariableInformation("c", b)
@@ -10,6 +9,11 @@ def test_Create_complex_VariableInformation_str():
     assert "a.b.c" == str(a)
     assert "b.c" == str(b)
     assert "c" == str(c)
+
+def test_Create_complex_VariableInformation_equality():
+    a = VariableInformation("a")
+    b = VariableInformation("b", a)
+    c = VariableInformation("c", b)
     assert a.child == b
     assert b.child == c
     assert c.child is None
