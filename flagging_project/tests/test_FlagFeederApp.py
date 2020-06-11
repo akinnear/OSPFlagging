c389507ee2419575bbd6aa428b443be5ebe72587
markfickman <mfickman@gmail.com>
2020-06-11 10:45:52 -0700

    #4: update test_multiplication_operation_CodeLocation

--- flagging_project/tests/test_FlagFeederApp.py
+++ flagging_project/tests/test_FlagFeederApp.py
@@ -211,7 +211,10 @@ def test_multiplication_operation_keys():
 test_1 = ff1 * ff2
 return ff3 < test_1"""
     test_output = determine_variables(logic)
-    assert test_output.used_variables.keys() == {"ff1", "ff2", "ff3", "test_1"}
+    assert test_output.used_variables.keys() == {VariableInformation("ff1", None),
+                                                 VariableInformation("ff2", None),
+                                                 VariableInformation("ff3", None),
+                                                 VariableInformation("test_1", None)}
     assert test_output.assigned_variables.keys() == {"test_1"}
     assert test_output.referenced_functions.keys() == set()
     assert test_output.defined_functions.keys() == set()
@@ -225,13 +228,16 @@ def test_multiplication_operation_CodeLocation():
 test_1 = ff1 * ff2
 return ff3 < test_1"""
     test_output = determine_variables(logic)
-    assert test_output.used_variables.keys() == {"ff1", "ff2", "ff3", "test_1"}
-    assert test_output.used_variables["ff1"] == {CodeLocation(line_number=2, column_offset=9)}
-    assert test_output.used_variables["ff2"] == {CodeLocation(line_number=2, column_offset=15)}
-    assert test_output.used_variables["ff3"] == {CodeLocation(line_number=3, column_offset=7)}
-    assert test_output.used_variables["test_1"] == {CodeLocation(line_number=3, column_offset=13)}
-    assert test_output.assigned_variables.keys() == {"test_1"}
-    assert test_output.assigned_variables == {'test_1': {CodeLocation(line_number=2, column_offset=0)}}
+    assert test_output.used_variables.keys() == {VariableInformation("ff1", None),
+                                                 VariableInformation("ff2", None),
+                                                 VariableInformation("ff3", None),
+                                                 VariableInformation("test_1", None)}
+    assert test_output.used_variables[VariableInformation("ff1", None)] == {CodeLocation(line_number=2, column_offset=9)}
+    assert test_output.used_variables[VariableInformation("ff2", None)] == {CodeLocation(line_number=2, column_offset=15)}
+    assert test_output.used_variables[VariableInformation("ff3", None)] == {CodeLocation(line_number=3, column_offset=7)}
+    assert test_output.used_variables[VariableInformation("test_1", None)] == {CodeLocation(line_number=3, column_offset=13)}
+    assert test_output.assigned_variables.keys() == {VariableInformation("test_1", None)}
+    assert test_output.assigned_variables == {VariableInformation('test_1', None): {CodeLocation(line_number=2, column_offset=0)}}
     assert test_output.referenced_functions.keys() == set()
     assert test_output.defined_functions.keys() == set()
     assert test_output.defined_classes.keys() == set()
