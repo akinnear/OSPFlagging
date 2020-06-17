e8c8212e8d8fe0a0ecc81156f9d831d58566fa36
markfickman <mfickman@gmail.com>
2020-06-16 22:16:56 -0700

    #33: update test_tuple_assignment_2_CodeLocation

--- flagging_project/tests/test_FlagFeederApp.py
+++ flagging_project/tests/test_FlagFeederApp.py
@@ -607,7 +607,7 @@ else:
     assert test_output.referenced_modules[ModuleInformation("math")] == {CodeLocation(2, 7)}
     assert test_output.referenced_flags.keys() == set()
 
-#TODO
+
 def test_tuple_assignment_2_CodeLocation():
     logic = """
 import math
@@ -660,8 +660,8 @@ else:
     assert test_output.referenced_functions[VariableInformation.create_var(["y_list", "add"])] == {CodeLocation(8, 8)}
     assert test_output.defined_functions.keys() == set()
     assert test_output.defined_classes.keys() == set()
-    assert test_output.referenced_modules.keys() == {"math"}
-    assert test_output.referenced_modules == {"math": {CodeLocation(2, 7)}}
+    assert test_output.referenced_modules.keys() == {ModuleInformation("math")}
+    assert test_output.referenced_modules == {ModuleInformation("math"): {CodeLocation(2, 7)}}
     assert test_output.referenced_flags.keys() == set()
 
 
