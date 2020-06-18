ccd734e8ebeeb972d2b36ba5e605f5fb6967d6bc
markfickman <mfickman@gmail.com>
2020-06-17 18:28:39 -0700

    #32: create test_special_lambda_CodeLocation

--- flagging_project/tests/test_FlagFeederApp.py
+++ flagging_project/tests/test_FlagFeederApp.py
@@ -1794,6 +1794,20 @@ y = m.sqrt(x)"""
     assert test_output.referenced_flags.keys() == set()
 
 
+def test_special_lambda_CodeLocation():
+    logic = """
+high_ord_func = lambda x, func: x + func(x)
+return high_ord_func(ff1, lambda x: x * x) > ff2"""
+    test_output = determine_variables(logic)
+    assert test_output.assigned_variables.keys() == set()
+    assert test_output.referenced_functions.keys() == set()
+    assert test_output.defined_functions.keys() == set()
+    assert test_output.defined_classes.keys() == set()
+    assert test_output.referenced_modules.keys() == set()
+    assert test_output.referenced_flags.keys() == set()
+
+
+
 def test_example():
     """Imagine code is:
     my_var = 1
