e4f5bd40325eb9ac2ba29b11be3be3fcb2f14664
markfickman <mfickman@gmail.com>
2020-07-08 13:35:07 -0700

    #52: update test_determine_flag_feeders_logic_and_CodeLocation

--- flagging_project/tests/test_FlagFeederApp.py
+++ flagging_project/tests/test_FlagFeederApp.py
@@ -20,7 +20,7 @@ def test_determine_flag_feeders_logic_and_CodeLocation():
     assert test_output.referenced_modules.keys() == set()
     assert test_output.referenced_flags.keys() == set()
     assert test_output.errors == []
-    assert len(test_output.validation_results.validation_errors) != 0
+    assert len(test_output.validation_results.validation_errors) == 0
 
 
 def test_determine_flag_feeders_logic_or_CodeLocation():
