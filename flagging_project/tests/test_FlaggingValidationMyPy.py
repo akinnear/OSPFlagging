cab86eb6cbf3d21bd4407dedaebd11572df5e901
markfickman <mfickman@gmail.com>
2020-07-17 12:09:28 -0700

    #52: continue mypy test creation

--- flagging_project/tests/test_FlaggingValidationMyPy.py
+++ flagging_project/tests/test_FlaggingValidationMyPy.py
@@ -116,21 +116,15 @@ def test_mypy_less_than_operation():
     assert test_output.warnings == {}
 
 
-
-def test_greater_than_operation_CodeLocation():
+#TODO
+# why does this pass?
+def test_mypy_greater_than_operation():
     logic = """return ff1 <= ff2"""
-    test_output = determine_variables(logic)
-    assert test_output.used_variables.keys() == {VariableInformation("ff1", None), VariableInformation("ff2", None)}
-    assert test_output.used_variables == {
-        VariableInformation("ff1", None): {CodeLocation(line_number=1, column_offset=7)},
-        VariableInformation("ff2", None): {CodeLocation(line_number=1, column_offset=14)}}
-    assert test_output.assigned_variables.keys() == set()
-    assert test_output.referenced_functions.keys() == set()
-    assert test_output.defined_functions.keys() == set()
-    assert test_output.defined_classes.keys() == set()
-    assert test_output.referenced_modules.keys() == set()
-    assert test_output.referenced_flags.keys() == set()
-    assert test_output.errors == []
+    flag_feeders = {"ff1": bool, "ff2": int}
+    test_output = validate_returns_boolean(determine_variables(logic), flag_feeders)
+    assert test_output.validation_errors == {}
+    assert test_output.other_errors == {}
+    assert test_output.warnings == {}
 
 
 def test_add_operation_CodeLocation():
