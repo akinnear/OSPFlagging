bc952bcfc27100236a227d751ebacccfda5d3659
markfickman <mfickman@gmail.com>
2020-07-20 13:08:50 -0700

    #52: create test_mypy_math_expression

--- flagging_project/tests/test_FlaggingValidationMyPy.py
+++ flagging_project/tests/test_FlaggingValidationMyPy.py
@@ -271,7 +271,8 @@ return ff1 == add"""
     assert test_output.validation_errors == {}
     assert test_output.warnings == {}
 
-def test_math_expression_CodeLocation():
+
+def test_mypy_math_expression():
     logic = """
 import math
 a_list = [10, 13, 16, 19, 22, -212]
@@ -280,51 +281,14 @@ for a in a_list:
     if math.sqrt(abs(a)) <= 4:
         y_list.append(int(math.sqrt(a)))
 if ff1 == (max(y_list)):
-    return ff1
+    return ff1 > max(a_list)
 else:
     return ff2 > min(list(map(lambda x: x**2, a_list)))"""
-    test_output = determine_variables(logic)
-    assert test_output.used_variables.keys() == {VariableInformation("a_list", None),
-                                                 VariableInformation("y_list", None),
-                                                 VariableInformation("a", None),
-                                                 VariableInformation("ff1", None),
-                                                 VariableInformation("ff2", None),
-                                                 VariableInformation("x", None)}
-    assert test_output.used_variables[VariableInformation("a_list", None)] == {CodeLocation(5, 9), CodeLocation(11, 46)}
-    assert test_output.used_variables[VariableInformation("y_list", None)] == {CodeLocation(7, 8), CodeLocation(8, 15)}
-    assert test_output.used_variables[VariableInformation("a", None)] == {CodeLocation(5, 4), CodeLocation(6, 21),
-                                                                          CodeLocation(7, 36)}
-    assert test_output.used_variables[VariableInformation("ff1", None)] == {CodeLocation(8, 3), CodeLocation(9, 11)}
-    assert test_output.used_variables[VariableInformation("ff2", None)] == {CodeLocation(11, 11)}
-    assert test_output.used_variables[VariableInformation("x", None)] == {CodeLocation(11, 40)}
-    assert test_output.assigned_variables.keys() == {VariableInformation("a_list", None),
-                                                     VariableInformation("y_list", None)}
-    assert test_output.assigned_variables == {VariableInformation("a_list", None): {CodeLocation(3, 0)},
-                                              VariableInformation("y_list", None): {CodeLocation(4, 0)}}
-    assert test_output.referenced_functions.keys() == {VariableInformation.create_var(["math", "sqrt"]),
-                                                       VariableInformation("abs", None),
-                                                       VariableInformation("max", None),
-                                                       VariableInformation("min", None),
-                                                       VariableInformation("list", None),
-                                                       VariableInformation("map", None),
-                                                       VariableInformation("int", None),
-                                                       VariableInformation.create_var(["y_list", "append"])}
-    assert test_output.referenced_functions[VariableInformation.create_var(["math", "sqrt"])] == {CodeLocation(6, 7),
-                                                                                                  CodeLocation(7, 26)}
-    assert test_output.referenced_functions[VariableInformation("abs", None)] == {CodeLocation(6, 17)}
-    assert test_output.referenced_functions[VariableInformation("max", None)] == {CodeLocation(8, 11)}
-    assert test_output.referenced_functions[VariableInformation("min", None)] == {CodeLocation(11, 17)}
-    assert test_output.referenced_functions[VariableInformation("list", None)] == {CodeLocation(11, 21)}
-    assert test_output.referenced_functions[VariableInformation("map", None)] == {CodeLocation(11, 26)}
-    assert test_output.referenced_functions[VariableInformation.create_var(["y_list", "append"])] == {
-        CodeLocation(7, 8)}
-    assert test_output.referenced_functions[VariableInformation('int', None)] == {CodeLocation(7, 22)}
-    assert test_output.defined_functions.keys() == set()
-    assert test_output.defined_classes.keys() == set()
-    assert test_output.referenced_modules.keys() == {ModuleInformation("math")}
-    assert test_output.referenced_modules == {ModuleInformation("math"): {CodeLocation(2, 7)}}
-    assert test_output.referenced_flags.keys() == set()
-    assert test_output.errors == []
+    flag_feeders = {"ff1": int, "ff2": int}
+    test_output = validate_returns_boolean(determine_variables(logic), flag_feeders)
+    assert test_output.other_errors == {}
+    assert test_output.validation_errors == {}
+    assert test_output.warnings == {}
 
 
 # TODO
