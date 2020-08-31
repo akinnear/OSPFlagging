a03bc8fa2ef0491641f2481d6508fd00dcb33876
markfickman <mfickman@gmail.com>
2020-08-31 11:44:36 -0700

    #67: mock flag feeder pull via pull_flag_feeders method

--- flagging_project/flag_feeders/flag_feeder_service.py
+++ flagging_project/flag_feeders/flag_feeder_service.py
@@ -3,10 +3,12 @@
 #function/method declartion
 
 #pull flag feeders via GET api call to endpoint or local storage location query
-def pull_flag_feeders (endpoint_storage_location):
+def pull_flag_feeders (*args, **kwargs):
     #TODO
     # valid data pull
+    api_endpoint = kwargs.get("api_endpoint", "mock_api_endpoint")
+    
     flag_feeders = {"FF1": str, "FF2": int, "FF3": bool, "FF4": float}
     return flag_feeders
- 
+
 
