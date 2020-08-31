c35690ff24076b2f66086140128bcfc4c6e361b2
markfickman <mfickman@gmail.com>
2020-08-31 11:13:17 -0700

    #67: mock flag feeder pull via pull_flag_feeders method

---
+++ flagging_project/flag_feeders/flag_feeder_service.py
@@ -0,0 +1,12 @@
+#import
+
+#function/method declartion
+
+#pull flag feeders via GET api call to endpoint or local storage location query
+def pull_flag_feeders (endpoint_storage_location):
+    #TODO
+    # valid data pull
+    flag_feeders = {"FF1": str, "FF2": int, "FF3": bool, "FF4": float}
+    return flag_feeders
+ 
+
