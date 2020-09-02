0600bd26768a70158277daf529b731472a89e7e0
markfickman <mfickman@gmail.com>
2020-09-02 14:56:11 -0700

    #67: create object to contain flagging schema service status and information to pass to user, via FlaggingSchemaInformation.py

---
+++ flagging_project/front_end/FlagingSchemaInformation.py
@@ -0,0 +1,35 @@
+class FlaggingSchemaInformation:
+
+    #was it succesful or not, pass
+    #error message
+    #flag_name
+    #UUID
+    #
+
+    def __init__(self, valid, message, name, uuid):
+        self.valid = valid
+        self.message = message
+        self.name = name
+        self.uuid = uuid
+
+
+
+    # object representation
+    def __repr__(self):
+        return f"FlaggingSchemaInformation({self.valid}, {self.message}, {self.name}, {self.uuid})"
+
+    def __str__(self):
+        return f"{self.valid}, {self.message}, {self.name}, {self.uuid}"
+
+    # object equality
+    def __eq__(self, other):
+        return self.valid == other.valid \
+               and self.message == other.message \
+               and self.name == other.name \
+               and self.uuid == other.uuid
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash((self.valid, self.message, self.name, self.uuid))
