cc4c9bd4f892fb8f8ce62d2f62cb0cdc4644cccf
markfickman <mfickman@gmail.com>
2020-09-03 13:18:51 -0700

    #66: add pull_flag_names_in_flag_group to FlagService.py (dummy pull for flag names inside of a flag group)

--- flagging_project/flag_names/FlagService.py
+++ flagging_project/flag_names/FlagService.py
@@ -10,3 +10,15 @@ def pull_flag_names(*args, **kwargs):
 
     if dummy_flag_names:
         return dummy_flag_names
+
+
+def pull_flag_names_in_flag_group(*args, **kwargs):
+    #method to pull falg names form api endpoint or intenal db query
+    #flags exists inside of flag_group
+
+    api_endpoint = kwargs.get("api_endpoint")
+
+    dummy_flag_names = kwargs.get("dummy_flag_names", None)
+
+    if dummy_flag_names:
+        return dummy_flag_names
