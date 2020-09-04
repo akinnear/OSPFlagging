5e398a18eff63ab40cbd67aa4da4caeb04c91811
markfickman <mfickman@gmail.com>
2020-09-04 10:41:58 -0700

    #66: create template for test_FlaggingSchemaService.py

---
+++ flagging_project/tests/test_FlaggingSchemaService.py
@@ -0,0 +1,60 @@
+from flag_names.FlagService import pull_flag_names, \
+    pull_flag_names_in_flag_group
+
+from flag_names.FlagGroupService import pull_flag_group_names
+
+from front_end.FlaggingSchemaService import validate_logic, get_flag_dependencies, \
+    create_flag, create_flag, update_flag_name, update_flag_logic, \
+    delete_flag, create_flag_group, delete_flag_group, add_flag_to_flag_group, \
+    remove_flag_from_flag_group, duplicate_flag, duplicate_flag_group
+
+#test validate logic
+
+
+#test get_flag_deps
+
+
+#test pull flags
+
+
+#test pull flag_groups
+
+
+#test pull flags in flag_group
+
+
+#test create flags
+
+
+
+#test update flag name
+
+
+#test update flag logic
+
+
+#test delete flag
+
+
+#test create flag group
+
+
+#test delete flag group
+
+
+#test add flag to flag group
+
+
+#test remove flag from flag group
+
+
+#test duplicate flag
+
+
+
+#test duplicate flag group
+
+
+
+
+
