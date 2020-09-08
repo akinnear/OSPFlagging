a4e53f5258d0d7ddf60092be423f813f5965c54f
markfickman <mfickman@gmail.com>
2020-09-08 16:40:26 -0700

    #66: create new test for FlaggingSchemaService

--- flagging_project/tests/test_FlaggingSchemaService.py
+++ flagging_project/tests/test_FlaggingSchemaService.py
@@ -27,10 +27,10 @@ def test_validation_user_defined_func_error(mock_determine_variables, mock_valid
 
 
 #test validate logic
-@mock.patch("front_end.FlaggingSchemaService.determine_variables", return_value=FlagLogicInformation(), autospec=True)
+# @mock.patch("front_end.FlaggingSchemaService.determine_variables", return_value=FlagLogicInformation(), autospec=True)
 @mock.patch("flagging.FlaggingValidation.validate_returns_boolean",
             return_value=TypeValidationResults(), autospec=True)
-def test_validate_logic_valid(mock_determine_variables, mock_validate_returns_boolean):
+def test_validate_logic_valid(mock_validate_returns_boolean):
     flag_id = "Flag_1A1A"
     flag_info = FlagLogicInformation(
         used_variables={VariableInformation('FF1'): {CodeLocation(1, 1)}}
@@ -41,10 +41,10 @@ def test_validate_logic_valid(mock_determine_variables, mock_validate_returns_bo
     assert result.warnings == {}
     assert result.mypy_warnings == {}
 
-@mock.patch("front_end.FlaggingSchemaService.determine_variables", return_value=FlagLogicInformation(), autospec=True)
+# @mock.patch("front_end.FlaggingSchemaService.determine_variables", return_value=FlagLogicInformation(), autospec=True)
 @mock.patch("flagging.FlaggingValidation.validate_returns_boolean",
             return_value=TypeValidationResults(validation_errors={"mock_validation_error": {CodeLocation(2, 2)}}), autospec=True)
-def test_validate_logic_w_exp_errors(mock_determine_variables, mock_validate_returns_boolean):
+def test_validate_logic_w_exp_errors(mock_validate_returns_boolean):
     flag_id = "Flag_1A1A"
     flag_info = FlagLogicInformation(
         used_variables={VariableInformation('FF1'): {CodeLocation(1, 1)}}
@@ -109,12 +109,201 @@ def test_validation_user_defined_func_error():
     assert result.message == "flag group FG1A1A has been updated with flag(s) Flag3"
     assert result.uuid == "FG1A1A_primary_key_id"
 
+#flag group does not exist
+def test_validation_flag_group_no_exist():
+    flag_group_id = "FG1A1A"
+    new_flags = ["Flag3"]
+    existing_flags = ["Flag1", "Flag2", "Flag3"]
+    existing_flag_groups = ['FG2B2B']
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1", "Flag2"])
+    result = add_flag_to_flag_group(flag_group_id=flag_group_id,
+                                    new_flags=new_flags,
+                                    existing_flags=existing_flags,
+                                    existing_flag_groups=existing_flag_groups,
+                                    flags_in_flag_group=flags_in_flag_group)
+
+    assert result.valid == False
+    assert result.message == "flag_group " + flag_group_id + " does not exist"
+    assert result.uuid == "FG1A1A_primary_key_id"
+
+
+#flag does not exist
+def test_validation_flag_group_flag_no_exist():
+    flag_group_id = "FG1A1A"
+    new_flags = ['Flag3', "Flag4"]
+    existing_flags = ["Flag1", "Flag2", "Flag3"]
+    existing_flag_groups = ["FG1A1A", "FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1", "Flag2"])
+    result = add_flag_to_flag_group(flag_group_id=flag_group_id,
+                                    new_flags=new_flags,
+                                    existing_flags=existing_flags,
+                                    existing_flag_groups=existing_flag_groups,
+                                    flags_in_flag_group=flags_in_flag_group)
+    print("hello")
+    assert result.valid == False
+    assert result.message == "Flag(s) Flag4 do not exist"
+    assert result.uuid == "FG1A1A_primary_key_id"
+
+
+def test_validation_flag_group_flag_no_exist_2():
+    flag_group_id = "FG1A1A"
+    new_flags = ['Flag3', "Flag4", "Flag5"]
+    existing_flags = ["Flag1", "Flag2", "Flag3"]
+    existing_flag_groups = ["FG1A1A", "FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1", "Flag2"])
+    result = add_flag_to_flag_group(flag_group_id=flag_group_id,
+                                    new_flags=new_flags,
+                                    existing_flags=existing_flags,
+                                    existing_flag_groups=existing_flag_groups,
+                                    flags_in_flag_group=flags_in_flag_group)
+
+    assert result.valid == False
+    assert result.message == "Flag(s) Flag4, Flag5 do not exist"
+    assert result.uuid == "FG1A1A_primary_key_id"
+
+
 
 
 #test remove flag from flag group
+def test_validation_remove_flag_from_flag_group():
+    flag_group_id = "FG1A1A"
+    existing_flags = ["Flag1", "Flag2", "Flag3"]
+    flags_2_remove = ["Flag2"]
+    existing_flag_groups = ["FG1A1A", "FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1", "Flag2"])
+    result = remove_flag_from_flag_group(flag_group_id=flag_group_id,
+                                         del_flags=flags_2_remove,
+                                         existing_flags=existing_flags,
+                                         existing_flag_groups=existing_flag_groups,
+                                         flags_in_flag_group=flags_in_flag_group)
+    assert result.valid == True
+    assert result.message == "Flag(s) Flag2 removed from FG1A1A"
+    assert result.uuid == "FG1A1A_primary_key_id"
 
+def test_validation_remove_flag_from_flag_group_2():
+    flag_group_id = "FG1A1A"
+    existing_flags = ["Flag1", "Flag2", "Flag3", "Flag4", "Flag5"]
+    flags_2_remove = ["Flag2", "Flag5"]
+    existing_flag_groups = ["FG1A1A", "FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1", "Flag2", "Flag5"])
+    result = remove_flag_from_flag_group(flag_group_id=flag_group_id,
+                                         del_flags=flags_2_remove,
+                                         existing_flags=existing_flags,
+                                         existing_flag_groups=existing_flag_groups,
+                                         flags_in_flag_group=flags_in_flag_group)
+    assert result.valid == True
+    assert result.message == "Flag(s) Flag2, Flag5 removed from FG1A1A"
+    assert result.uuid == "FG1A1A_primary_key_id"
+
+def test_validation_remove_flag_from_flag_group_no_flag_group():
+    flag_group_id = "FG1A1A"
+    existing_flags = ["Flag1", "Flag2", "Flag3"]
+    flags_2_remove = ["Flag2"]
+    existing_flag_groups = ["FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1", "Flag2"])
+    result = remove_flag_from_flag_group(flag_group_id=flag_group_id,
+                                         del_flags=flags_2_remove,
+                                         existing_flags=existing_flags,
+                                         existing_flag_groups=existing_flag_groups,
+                                         flags_in_flag_group=flags_in_flag_group)
+    assert result.valid == False
+    assert result.message == "flag_group " + flag_group_id + " does not exist"
+    assert result.uuid == "FG1A1A_primary_key_id"
+
+#test removal of flags that do not exist in flag group
+def test_validation_remove_flag_from_flag_group_no_flag():
+    flag_group_id = "FG1A1A"
+    existing_flags = ["Flag1", "Flag2", "Flag3"]
+    flags_2_remove = ["Flag2"]
+    existing_flag_groups = ["FG1A1A", "FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1"])
+    result = remove_flag_from_flag_group(flag_group_id=flag_group_id,
+                                         del_flags=flags_2_remove,
+                                         existing_flags=existing_flags,
+                                         existing_flag_groups=existing_flag_groups,
+                                         flags_in_flag_group=flags_in_flag_group)
+    assert result.valid == False
+    assert result.message == "Flag(s) Flag2 do not exist in FG1A1A"
+    assert result.uuid == "FG1A1A_primary_key_id"
+
+def test_validation_remove_flag_from_flag_group_no_flag_2():
+    flag_group_id = "FG1A1A"
+    existing_flags = ["Flag1", "Flag2", "Flag3"]
+    flags_2_remove = ["Flag2", "Flag3"]
+    existing_flag_groups = ["FG1A1A", "FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1"])
+    result = remove_flag_from_flag_group(flag_group_id=flag_group_id,
+                                         del_flags=flags_2_remove,
+                                         existing_flags=existing_flags,
+                                         existing_flag_groups=existing_flag_groups,
+                                         flags_in_flag_group=flags_in_flag_group)
+    assert result.valid == False
+    assert result.message == "Flag(s) Flag2, Flag3 do not exist in FG1A1A"
+    assert result.uuid == "FG1A1A_primary_key_id"
+
+#test removal of flags from flag group where flags are not defined
+def test_validation_remove_flag_from_flag_group_missing_flag():
+    flag_group_id = "FG1A1A"
+    existing_flags = ["Flag1", "Flag3"]
+    flags_2_remove = ["Flag2"]
+    existing_flag_groups = ["FG1A1A", "FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1"])
+    result = remove_flag_from_flag_group(flag_group_id=flag_group_id,
+                                         del_flags=flags_2_remove,
+                                         existing_flags=existing_flags,
+                                         existing_flag_groups=existing_flag_groups,
+                                         flags_in_flag_group=flags_in_flag_group)
+    assert result.valid == False
+    assert result.message == "Flag(s) Flag2 do not exist"
+    assert result.uuid == "FG1A1A_primary_key_id"
+
+def test_validation_remove_flag_from_flag_group_missing_flag_2():
+    flag_group_id = "FG1A1A"
+    existing_flags = ["Flag1"]
+    flags_2_remove = ["Flag2", "Flag3"]
+    existing_flag_groups = ["FG1A1A", "FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1"])
+    result = remove_flag_from_flag_group(flag_group_id=flag_group_id,
+                                         del_flags=flags_2_remove,
+                                         existing_flags=existing_flags,
+                                         existing_flag_groups=existing_flag_groups,
+                                         flags_in_flag_group=flags_in_flag_group)
+    assert result.valid == False
+    assert result.message == "Flag(s) Flag2, Flag3 do not exist"
+    assert result.uuid == "FG1A1A_primary_key_id"
 
 #test duplicate flag
+def test_validation_flag_group_duplicate_flag():
+    flag_group_id = "FG1A1A"
+    new_flags = ["Flag3"]
+    existing_flags = ['Flag1', "Flag2", "Flag3"]
+    existing_flag_groups = ["FG1A1A", "FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1", "Flag3"])
+    result = add_flag_to_flag_group(flag_group_id=flag_group_id,
+                                    new_flags=new_flags,
+                                    existing_flags=existing_flags,
+                                    existing_flag_groups=existing_flag_groups,
+                                    flags_in_flag_group=flags_in_flag_group)
+    print('hello')
+    assert result.valid == False
+    assert result.message == "Flag(s) Flag3 already exist in flag group"
+    assert result.uuid == "FG1A1A_primary_key_id"
+
+def test_validation_flag_group_duplicate_flag_2():
+    flag_group_id = "FG1A1A"
+    new_flags = ["Flag3", "Flag4"]
+    existing_flags = ['Flag1', "Flag2", "Flag3", "Flag4"]
+    existing_flag_groups = ["FG1A1A", "FG2B2B"]
+    flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["Flag1", "Flag3", "Flag4"])
+    result = add_flag_to_flag_group(flag_group_id=flag_group_id,
+                                    new_flags=new_flags,
+                                    existing_flags=existing_flags,
+                                    existing_flag_groups=existing_flag_groups,
+                                    flags_in_flag_group=flags_in_flag_group)
+    print('hello')
+    assert result.valid == False
+    assert result.message == "Flag(s) Flag3, Flag4 already exist in flag group"
+    assert result.uuid == "FG1A1A_primary_key_id"
 
 
 
