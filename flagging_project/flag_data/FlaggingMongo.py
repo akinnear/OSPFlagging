26372de341b306dde37aa5f585bdabb8601e403c
markfickman <mfickman@gmail.com>
2020-09-15 14:19:31 -0700

    #72: add function to add and get flag dependency data stored in separate table

--- flagging_project/flag_data/FlaggingMongo.py
+++ flagging_project/flag_data/FlaggingMongo.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 
 FLAGGING_DATABASE = 'flagging_test'
 FLAGGING_COLLECTION = 'flagging'
+FLAG_DEPENDENCIES = "flag_dependencies"
 
 
 class FlaggingMongo:
@@ -18,6 +19,17 @@ class FlaggingMongo:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.client.close()
 
+    #TODO,
+    # define data structure for flag
+    # Flag ID (default, do not need to specify), Flag Name,
+    # validtion_results.errors, validation_results.mypy_errors
+    # referenced_flags,
+    # Flag Update Date
+    # set up index on flag name
+
+
+
+
     def add_flag(self, flag):
         db = self.client[FLAGGING_DATABASE]
         flagging = db[FLAGGING_COLLECTION]
@@ -31,3 +43,14 @@ class FlaggingMongo:
         db = self.client[FLAGGING_DATABASE]
         flagging = db[FLAGGING_COLLECTION]
         return list(flagging.find())
+
+
+    def add_flag_dependencies(self, flag_deps):
+        db = self.client[FLAGGING_DATABASE]
+        flagging = db[FLAG_DEPENDENCIES]
+        flagging.insert_one(flag_deps)
+
+    def get_flag_dependencies(self):
+        db = self.client[FLAGGING_DATABASE]
+        flagging_dependencies = db[FLAG_DEPENDENCIES]
+        return list(flagging_dependencies.find())
