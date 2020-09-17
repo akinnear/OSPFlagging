f46595ef207c607c12165255c70f8a64dad4e035
markfickman <mfickman@gmail.com>
2020-09-17 16:17:07 -0700

    #72: layout data models and debugg add_specific_flag_dependencies.

--- flagging_project/flag_data/FlaggingMongo.py
+++ flagging_project/flag_data/FlaggingMongo.py
@@ -2,6 +2,7 @@ from typing import Callable
 
 from pymongo import MongoClient
 import datetime
+import pandas as pd
 
 FLAGGING_DATABASE = 'flagging_test'
 FLAGGING_COLLECTION = 'flagging'
@@ -21,15 +22,20 @@ class FlaggingMongo:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.client.close()
 
-    #TODO,
-    # define data structure for flag
-    # Flag ID (default, do not need to specify), Flag Name,
-    # validtion_results.errors, validation_results.mypy_errors
-    # referenced_flags,
-    # Flag Update Date
+    #TODO
     # set up index on flag name
 
+
     #flags
+    '''
+    FLAGS
+    _id: unique user id
+    FLAG_NAME: common flag_name -> str
+    FLAG_VALIDATION_RESULTS: results of flag validation (errors.__str__(), mypy errors.__str__())
+    REFERENCED_FLAGS: flags in flag logic -> [str]
+    FLAG_STATUS: is flag in production or in draft status -> bool
+    UPDATE_TIMESTAMP: datetime flag was last updated -> datetime object
+    '''
     def get_flags(self):
         db = self.client[FLAGGING_DATABASE]
         flagging = db[FLAGGING_COLLECTION]
@@ -64,6 +70,14 @@ class FlaggingMongo:
 
 
     #flag groups
+    '''
+    FLAG_GROUPS
+    _id: unique user id
+    FLAG_GROUP_NAME: flag group name -> str
+    FLAGS_IN_GROUP: flags in flag group -> [str]
+    FLAG_GROUP_STATUS: is flag_group ready for production or in development -> bool
+    UPDATE_TIMESTAMP: datetime flag group was last updated -> datetime object
+    '''
     def get_flag_groups(self):
         db = self.client[FLAGGING_DATABASE]
         flag_groups = db[FLAG_GROUPS]
@@ -98,6 +112,13 @@ class FlaggingMongo:
 
 
     #flag dependencies
+    '''
+    FLAG_DEPENDENCIES
+    _id: unique user id 
+    FLAG_NAME: flag_name -> str
+    DEPENDENT_FLAGS: flags that flag_name is dependent on -> [str]
+    UPDATE_TIMESTAMP: datetime flag dependency was last updated -> datetime object
+    '''
     def get_flag_dependencies(self):
         db = self.client[FLAGGING_DATABASE]
         flagging_dependencies = db[FLAG_DEPENDENCIES]
@@ -112,6 +133,45 @@ class FlaggingMongo:
         db = self.client[FLAGGING_DATABASE]
         flagging_dependencies = db[FLAG_DEPENDENCIES]
         flagging_dependencies.delete_one(query)
+
+    def get_specific_flag_dependencies(self, flag):
+        db = self.client[FLAGGING_DATABASE]
+        flagging_dependencies = db[FLAG_DEPENDENCIES]
+        flagging_dependencies = flagging_dependencies.find_one({"FLAG_NAME": flag})
+        return list(flagging_dependencies)
+
+
+    def add_specific_flag_dependencies(self, flag, new_deps: []):
+        db = self.client[FLAGGING_DATABASE]
+        flagging_dependencies = db[FLAG_DEPENDENCIES]
+        flag_deps_flag = flagging_dependencies.find_one({"FLAG_NAME": flag}, {"DEPENDENT_FLAGS": 1})["DEPENDENT_FLAGS"][0]
+        flag_deps_list = []
+        if isinstance(flag_deps_flag, list):
+            for x in flag_deps_flag:
+                flag_deps_list.append(x)
+        else:
+            flag_deps_list.append(flag_deps_flag)
+        for new_dep in new_deps:
+            if new_dep not in flag_deps_list:
+                flag_deps_list.append(new_dep)
+        updated_id = flagging_dependencies.find_and_modify(query={"FLAG_NAME": flag}, remove=False, update={"DEPENDENT_FLAGS": flag_deps_list})["_id"]
+        return updated_id
+
+    def remove_specific_flag_dependencies(self, flag, rm_deps: []):
+        db = self.client[FLAGGING_DATABASE]
+        flagging_dependencies = db[FLAG_DEPENDENCIES]
+        flag_deps_flag = list(flagging_dependencies.find_on({"FLAG_NAME": flag}))
+        df_flagging_dependencies = pd.DataFrame(list(flagging_dependencies.find()))
+        df_flag_deps_flag = df_flagging_dependencies[df_flagging_dependencies["FLAG_NAME"] == flag]
+        for rm_dep in rm_deps:
+            if not df_flag_deps_flag[df_flag_deps_flag["FLAG_DEPENDENCIES"].astype(str).str.contains(rm_dep)].empty:
+                for item in df_flag_deps_flag.items():
+                    item[1].remove(rm_dep)
+        item[1] = set(item[1])
+        item[1] = list(item[1])
+        flag_id = flagging_dependencies.find_and_modify(query={"FLAG_NAME": flag}, remove=False, update=item[1])["_id"]
+        return flag_id
+
 
 
     #def update_flag_depenency
