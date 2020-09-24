ab3cd27f57091c67a985232f9e4e3c919a1a1696
markfickman <mfickman@gmail.com>
2020-09-24 13:26:19 -0700

    #73: update test_FlaggingSchemaService.py based on new interfaces for get_flag_dependencies and validate_logic

--- flagging_project/tests/test_FlaggingSchemaService.py
+++ flagging_project/tests/test_FlaggingSchemaService.py
@@ -1,8 +1,10 @@
 from flag_names.FlagService import pull_flag_names, \
     pull_flag_names_in_flag_group
 from flag_names.FlagGroupService import pull_flag_group_names
-from front_end.FlaggingSchemaService import validate_logic, get_flag_dependencies, \
-    create_flag, create_flag, update_flag_name, update_flag_logic, \
+from front_end.FlaggingDependencies import get_flag_dependencies
+from front_end.FlaggingValidateLogic import validate_logic
+from front_end.FlaggingSchemaService import create_flag, create_flag, \
+    update_flag_name, update_flag_logic, \
     delete_flag, create_flag_group, delete_flag_group, add_flag_to_flag_group, \
     remove_flag_from_flag_group, duplicate_flag, duplicate_flag_group
 from flagging.FlagLogicInformation import FlagLogicInformation
