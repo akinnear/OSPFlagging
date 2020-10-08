cd21ea2352005cbaa27682543334dff902a1175c
markfickman <mfickman@gmail.com>
2020-10-08 13:37:25 -0700

    #76: build out api endpoints for flag actions

--- flagging_project/api_service/FlaggingService.py
+++ flagging_project/api_service/FlaggingService.py
@@ -23,33 +23,58 @@ def _create_flagging_mongo(container):
 
 container = MongoDbContainer(MONGO_DOCKER_IMAGE)
 app.config["MONGO_URI"] = _create_flagging_mongo(container)
-mongo = PyMongo(app)
+flagging_mongo = PyMongo(app)
 
 #flags
-@app.route("/flag/<string:function>/", methods=["GET", "POST", "PUT"])
-def flag_action(function=None):
+@app.route("/flag/", methods=["GET"])
+@app.route("/flag/<string:function>/<string:flag_id>/", methods=["GET", "POST", "PUT"])
+@app.route("/flag/<string:function>/<string:flag_name>/", methods=["GET", "POST", "PUT"])
+@app.route("/flag/<string:function>/<string:flag_id>/<string:flag_name>/", methods=["GET", "POST", "PUT"])
+def flag_action(function=None, flag_id=None, flag_name=None):
     if function is None:
         #route to flag home pag
         return "flag_home_page"
+
     else:
         if function == "get_all_flags":
-            get_all_flags
+            return(get_all_flags(flagging_mongo))
+
         if function == "get_specific_flag":
-            get_specific_flag
+            existing_flags = get_all_flags(flagging_mongo)
+            return(get_specific_flag(flag_id, existing_flags, flagging_mongo))
+
         if function == "create_flag":
-            create_flag
+            #TODO
+            # need to pull flag logic,
+            # need method and function to return flag logic without direct reference
+            # to mongo db in function
+            return(create_flag(flag_name, FlagLogicInformation(), flagging_mongo))
+
         if function == "update_flag_name":
-            update_flag_name
+            existing_flags = get_all_flags(flagging_mongo)
+            return(update_flag_name(flag_id, flag_name, existing_flags, flagging_mongo))
+
         if function == "update_flag_logic":
-            update_flag_logic
+            #TODO
+            # need to pull flag logic,
+            # need method and function to return flag logic without direct reference
+            # to mongo db in function
+            existing_flags = get_all_flags(flagging_mongo)
+            return(update_flag_logic(flag_id, FlagLogicInformation(), existing_flags, flagging_mongo))
+
         if function == "delete_flag":
-            delete_flag
+            existing_flags = get_all_flags(flagging_mongo)
+            return(delete_flag(flag_id, existing_flags, flagging_mongo))
+
         if function == "duplicate_flag":
-            duplicate_flag
+            existing_flags = get_all_flags(flagging_mongo)
+            return(duplicate_flag(flag_id, existing_flags, flagging_mongo))
+
         else:
              return "flag_home_page"
 
 #flag groups
+@app.route("/flag_group/", methods=["GET"])
 @app.route("/flag_group/<string:function>/", methods=["GET", "POST", "PUT"])
 def flag_group_action(function=None):
     if function is None:
@@ -74,6 +99,7 @@ def flag_group_action(function=None):
              return "flag_group_home_page"
 
 #flag_dependency
+@app.route("/flag_dependency/", methods=["GET"])
 @app.route("/flag_dependency/<string:function>/", methods=["GET", "POST", "PUT"])
 def flag_dependency_action(function=None):
     if function is None:
