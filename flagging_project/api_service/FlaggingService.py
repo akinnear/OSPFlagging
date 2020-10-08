a00cef33586dc6112ef21e22d0a165cf51c9e13b
markfickman <mfickman@gmail.com>
2020-10-08 14:17:03 -0700

    #76: build out api endpoints for flag group actions

--- flagging_project/api_service/FlaggingService.py
+++ flagging_project/api_service/FlaggingService.py
@@ -12,6 +12,7 @@ from pymongo import MongoClient
 from testcontainers.mongodb import MongoDbContainer
 from flagging_project.flag_data.FlaggingMongo import FlaggingMongo, FLAGGING_DATABASE, FLAGGING_COLLECTION, FLAG_DEPENDENCIES, FLAG_GROUPS
 from api_service import MONGO_DOCKER_IMAGE
+from flag_names.FlagService import pull_flag_names_in_flag_group
 
 
 #configure pymongo to integrate with flask
@@ -59,6 +60,7 @@ def flag_action(function=None, flag_id=None, flag_name=None):
             # need to pull flag logic,
             # need method and function to return flag logic without direct reference
             # to mongo db in function
+
             existing_flags = get_all_flags(flagging_mongo)
             return(update_flag_logic(flag_id, FlagLogicInformation(), existing_flags, flagging_mongo))
 
@@ -76,25 +78,60 @@ def flag_action(function=None, flag_id=None, flag_name=None):
 #flag groups
 @app.route("/flag_group/", methods=["GET"])
 @app.route("/flag_group/<string:function>/", methods=["GET", "POST", "PUT"])
-def flag_group_action(function=None):
+@app.route("/flag_group/<string:function>/<string:flag_group_id>/", methods=["GET", "POST", "PUT"])
+@app.route("/flag_group/<string:function>/<string:flag_group_name>/", methods=["GET", "POST", "PUT"])
+@app.route("/flag_group/<string:function>/<string:flag_group_id>/<string:flag_group_name>/", methods=["GET", "POST", "PUT"])
+def flag_group_action(function=None, flag_group_id=None, flag_group_name=None):
     if function is None:
         #route to flag home pag
         return "flag_group_home_page"
+
     else:
         if function == "get_flag_groups":
-            get_flag_groups
+            return(get_flag_groups(flagging_mongo))
+
         if function == "get_specific_flag_group":
-            get_specific_flag_group
+            existing_flag_groups=get_flag_groups(flagging_mongo)
+            return(get_specific_flag_group(flag_group_id, existing_flag_groups, flagging_mongo))
+
         if function == "create_flag_group":
-            create_flag_group
+            existing_flag_groups=get_flag_groups(flagging_mongo)
+            return(create_flag_group(flag_group_name, existing_flag_groups, flagging_mongo))
+
         if function == "delete_flag_group":
-            delete_flag_group
+            existing_flag_groups=get_flag_groups(flagging_mongo)
+            return(delete_flag_group(flag_group_id, existing_flag_groups, flagging_mongo))
+
         if function == "add_flag_to_flag_group":
-            add_flag_to_flag_group
+            #TODO
+            # need method and function
+            # to get existing flags in flag groups
+            # and flags to be added (new flags)
+            new_flags = []
+            flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["FLAG1A"])
+            existing_flags = get_all_flags(flagging_mongo)
+            existing_flag_groups = get_flag_groups(flagging_mongo)
+            return(add_flag_to_flag_group(flag_group_id=flag_group_id, new_flags=new_flags,
+                                          existing_flags=existing_flags, existing_flag_groups=existing_flag_groups,
+                                          flags_in_flag_group=flags_in_flag_group, flagging_mongo=flagging_mongo))
+
         if function == "remove_flag_from_flag_group":
-            remove_flag_from_flag_group
+            #TODO
+            # need method and function
+            # to get existing flags in flag groups
+            # and flags to be removed (del_flags)
+            del_flags = []
+            flags_in_flag_group = pull_flag_names_in_flag_group(dummy_flag_names=["FLAG1A"])
+            existing_flags = get_all_flags(flagging_mongo)
+            existing_flag_groups = get_flag_groups(flagging_mongo)
+            return(remove_flag_from_flag_group(flag_group_id=flag_group_id, del_flags=del_flags,
+                                               existing_flags=existing_flags, existing_flag_groups=existing_flag_groups,
+                                               flags_in_flag_group=flags_in_flag_group, flagging_mongo=flagging_mongo))
+
         if function == "duplicate_flag_group":
-            duplicate_flag_group
+            existing_flag_groups = get_flag_groups(flagging_mongo)
+            return(duplicate_flag_group(flag_group_id, existing_flag_groups, flagging_mongo))
+        
         else:
              return "flag_group_home_page"
 
