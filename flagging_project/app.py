3eff594135c252f2e23fbc0c444a0cce24f45441
markfickman <mfickman@gmail.com>
2020-10-15 11:54:49 -0700

    #76: re work flag api endpoings , keep everything in same app file for the time being, break out via blueprints and views in the future

--- flagging_project/app.py
+++ flagging_project/app.py
@@ -1,17 +1,22 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
 from front_end.FlaggingSchemaService import get_all_flags, get_specific_flag, \
     get_flag_groups, get_specific_flag_group, get_flag_dependencies, get_specif_flag_dependnecy, \
     create_flag, update_flag_name, update_flag_logic, delete_flag, create_flag_group, \
     delete_flag_group, add_flag_to_flag_group, remove_flag_from_flag_group, duplicate_flag, \
     duplicate_flag_group, create_flag_dependency, delete_flag_dependency, add_dependencies_to_flag, \
-    remove_dependencies_from_flag
+    remove_dependencies_from_flag, get_all_flag_ids
 from flagging.FlagLogicInformation import FlagLogicInformation
 from flag_names.FlagService import pull_flag_names_in_flag_group, pull_flag_names, pull_flag_logic_information
 from flag_data.FlaggingMongo import FlaggingMongo
+# from api_service.FlaggingService import flag_api
+from bson.objectid import ObjectId
+import json
 
-#intialize app
 app = Flask(__name__)
+# app.register_blueprint(flag_api)
+
+
 
 #configure secret key
 app.secret_key = os.urandom(24).hex()
@@ -19,52 +24,77 @@ app.secret_key = os.urandom(24).hex()
 def _create_flagging_mongo():
     return FlaggingMongo("mongodb://localhost:27017/flagging")
 
+def _make_json_pretty(json_data):
+    json_data = jsonify(json_data)
+    json_object = json.loads(json_data)
+
+    json_formatted_str = json.dumps(json_object, indent=2)
+    return json_formatted_str
+
 #hello world
 @app.route('/')
 def hello_world():
     return "Hello, World!"
 
+@app.route("/FLAG")
+def flag_home_page():
+    return "flag home page to go here"
+
+@app.route("/FLAG_GROUP")
+def flag_group_home_page():
+    return "flag group home page to go here"
+
+@app.route("/FLAG_DEPENDENCIES")
+def flag_dependencies_home_page():
+    return "flag dependencies home page to go here"
+
 
 #flags
-@app.route("/flag/", methods=["GET"])
-@app.route("/flag/<string:function>/", methods=["GET", "POST", "PUT"])
-@app.route("/flag/<string:function>/<string:flag_id>/", methods=["GET", "POST", "PUT"])
-@app.route("/flag/<string:function>/<string:flag_name>/", methods=["GET", "POST", "PUT"])
-@app.route("/flag/<string:function>/<string:flag_id>/<string:flag_name>/", methods=["GET", "POST", "PUT"])
+@app.route("/flag", methods=["GET"])
+@app.route("/flag/<string:function>", methods=["GET", "POST", "PUT"])
+@app.route("/flag/<string:function>/<string:flag_id>", methods=["GET"])
+@app.route("/flag/<string:function>/<string:flag_id>/<string:flag_name>", methods=["GET", "POST", "PUT"])
 def flag_action(function=None, flag_id=None, flag_name=None):
     if function is None:
         #route to flag home page
-        return "flag_home_page"
+        return redirect(flag_home_page)
 
     else:
         if function == "get_all_flags":
             flags = get_all_flags(flagging_mongo)
+            flags = [str(x) for x in flags]
             return jsonify({'flags': flags})
 
 
-        if function == "get_specific_flag":
-            existing_flags = get_all_flags(flagging_mongo)
-            return(get_specific_flag(flag_id, existing_flags, flagging_mongo))
 
+        if function == "get_specific_flag":
+            existing_flag_ids = get_all_flag_ids(flagging_mongo)
+            flag_id_object = ObjectId(flag_id)
+            flag_schema_object = get_specific_flag(flag_id_object, existing_flag_ids, flagging_mongo)
+            return jsonify(({"valid": flag_schema_object.valid,
+                             "message": flag_schema_object.message,
+                             "uuid": str(flag_schema_object.uuid)}))
 
         if function == "create_flag":
             #TODO
             # need to pull flag logic,
             # need method and function to return flag logic without direct reference
             # to mongo db in function
-            try:
-                flag_info = pull_flag_logic_information(dummy_flag=True)
-                flag_schema_object = create_flag(flag_name, flag_info, flagging_mongo)
-                print('hello')
-                return(jsonify({"new uuid": str(flag_schema_object.uuid),
-                                "flagging_mongo_url": flagging_mongo.connection_url}))
-            except Exception as e:
-                return("error: " + str(e))
+
+            flag_info = pull_flag_logic_information(dummy_flag=True)
+            flag_schema_object = create_flag(flag_name, flag_info, flagging_mongo)
+            return jsonify({"valid": flag_schema_object.valid,
+                             "message": flag_schema_object.message,
+                             "uuid": str(flag_schema_object.uuid)})
 
 
         if function == "update_flag_name":
-            existing_flags = get_all_flags(flagging_mongo)
-            return(update_flag_name(flag_id, flag_name, existing_flags, flagging_mongo))
+            existing_flag_ids = get_all_flag_ids(flagging_mongo)
+            flag_id_object = ObjectId(flag_id)
+            flag_schema_object = update_flag_name(flag_id_object, flag_name, existing_flag_ids, flagging_mongo)
+            return jsonify({"valid": flag_schema_object.valid,
+                             "message": flag_schema_object.message,
+                             "uuid": str(flag_schema_object.uuid)})
 
         if function == "update_flag_logic":
             #TODO
@@ -72,19 +102,31 @@ def flag_action(function=None, flag_id=None, flag_name=None):
             # need method and function to return flag logic without direct reference
             # to mongo db in function
 
-            existing_flags = get_all_flags(flagging_mongo)
-            return(update_flag_logic(flag_id, FlagLogicInformation(), existing_flags, flagging_mongo))
+            existing_flag_ids = get_all_flag_ids(flagging_mongo)
+            flag_info = pull_flag_logic_information(dummy_flag=True)
+            flag_id_object = ObjectId(flag_id)
+            flag_schema_object = update_flag_logic(flag_id_object, flag_info, existing_flag_ids, flagging_mongo)
+            return jsonify({"valid": flag_schema_object.valid,
+                             "message": flag_schema_object.message,
+                             "uuid": str(flag_schema_object.uuid)})
 
         if function == "delete_flag":
-            existing_flags = get_all_flags(flagging_mongo)
-            return(delete_flag(flag_id, existing_flags, flagging_mongo))
+            existing_flag_ids = get_all_flag_ids(flagging_mongo)
+            flag_id_object = ObjectId(flag_id)
+            flag_schema_object = delete_flag(flag_id_object, existing_flag_ids, flagging_mongo)
+            return jsonify({"valid": flag_schema_object.valid,
+                             "message": flag_schema_object.message,
+                             "uuid": str(flag_schema_object.uuid)})
 
         if function == "duplicate_flag":
-            existing_flags = get_all_flags(flagging_mongo)
-            return(duplicate_flag(flag_id, existing_flags, flagging_mongo))
-
+            existing_flag_ids = get_all_flag_ids(flagging_mongo)
+            flag_id_object = ObjectId(flag_id)
+            flag_schema_object = duplicate_flag(flag_id_object, existing_flag_ids, flagging_mongo)
+            return jsonify({"valid": flag_schema_object.valid,
+                             "message": flag_schema_object.message,
+                             "uuid": str(flag_schema_object.uuid)})
         else:
-             return "flag_home_page"
+             return redirect("/FLAG")
 
 #flag groups
 @app.route("/flag_group/", methods=["GET"])
