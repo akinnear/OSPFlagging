544a151d478f7109ae4a043b7f1eb92439b23aa7
markfickman <mfickman@gmail.com>
2020-10-17 14:28:34 -0700

    #76: change data key word flag logic information

--- flagging_project/front_end/FlaggingSchemaInformation.py
+++ flagging_project/front_end/FlaggingSchemaInformation.py
@@ -1,21 +1,21 @@
 class FlaggingSchemaInformation:
 
 
-    def __init__(self, valid, message, name=None, uuid=None, data=None):
+    def __init__(self, valid, message, name=None, uuid=None, flag_logic_information=None):
         self.valid = valid
         self.message = message
         self.name = name
         self.uuid = uuid
-        self.data = data
+        self.flag_logic_information = flag_logic_information
 
 
 
     # object representation
     def __repr__(self):
-        return f"FlaggingSchemaInformation({self.valid}, {self.message}, {self.name}, {self.uuid}, {self.data})"
+        return f"FlaggingSchemaInformation({self.valid}, {self.message}, {self.name}, {self.uuid}, {self.flag_logic_information})"
 
     def __str__(self):
-        return f"{self.valid}, {self.message}, {self.name}, {self.uuid}", {self.data}
+        return f"{self.valid}, {self.message}, {self.name}, {self.uuid}", {self.flag_logic_information}
 
     # object equality
     def __eq__(self, other):
@@ -23,10 +23,10 @@ class FlaggingSchemaInformation:
                and self.message == other.message \
                and self.name == other.name \
                and self.uuid == other.uuid \
-               and self.data == other.data
+               and self.flag_logic_information == other.flag_logic_information
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
     def __hash__(self):
-        return hash((self.valid, self.message, self.name, self.uuid, self.data))
+        return hash((self.valid, self.message, self.name, self.uuid, self.flag_logic_information))
