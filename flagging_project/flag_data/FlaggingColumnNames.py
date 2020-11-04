b462d082331c47544f15e925c5f0c09a741c2309
markfickman <mfickman@gmail.com>
2020-11-04 13:49:28 -0800

    #76: new column names to pass

--- flagging_project/flag_data/FlaggingColumnNames.py
+++ flagging_project/flag_data/FlaggingColumnNames.py
@@ -10,3 +10,4 @@ flag_group_status_col_name = "FLAG_GROUP_STATUS"
 
 flag_dep_dep_flags_col_name = "DEPENDENT_FLAGS"
 flag_dep_flag_id_col_name = "FLAG_ID"
+flag_dep_flag_group_id_col_name = "FLAG_GROUP_ID"
