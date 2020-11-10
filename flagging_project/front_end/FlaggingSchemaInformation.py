f5abd8e946ca48a757d01f1e3ba8aee560d78295
markfickman <mfickman@gmail.com>
2020-11-10 13:53:47 -0800

    #76: update FlaggingSchemaInformation.py to contain simple_message attribute

--- flagging_project/front_end/FlaggingSchemaInformation.py
+++ flagging_project/front_end/FlaggingSchemaInformation.py
@@ -1,8 +1,9 @@
 class FlaggingSchemaInformation:
 
 
-    def __init__(self, valid, message, name=None, uuid=None, logic=None):
+    def __init__(self, valid, simple_message="", message="", name=None, uuid=None, logic=None):
         self.valid = valid
+        self.simple_message = simple_message
         self.message = message
         self.name = name
         self.uuid = uuid
@@ -12,14 +13,17 @@ class FlaggingSchemaInformation:
 
     # object representation
     def __repr__(self):
-        return f"FlaggingSchemaInformation({self.valid}, {self.message}, {self.name}, {self.uuid}, {self.logic})"
+        return f"FlaggingSchemaInformation({self.valid}, {self.simple_message}, {self.message}, {self.name}, " \
+               f"{self.uuid}, {self.logic})"
 
     def __str__(self):
-        return f"{self.valid}, {self.message}, {self.name}, {self.uuid}", {self.logic}
+        return f"{self.valid}, {self.simple_message}, {self.message}, " \
+               f"{self.name}, {self.uuid}", {self.logic}
 
     # object equality
     def __eq__(self, other):
         return self.valid == other.valid \
+               and self.simple_message == other.simple_message \
                and self.message == other.message \
                and self.name == other.name \
                and self.uuid == other.uuid \
@@ -29,4 +33,5 @@ class FlaggingSchemaInformation:
         return not self.__eq__(other)
 
     def __hash__(self):
-        return hash((self.valid, self.message, self.name, self.uuid, self.logic))
+        return hash((self.valid, self.simple_message, self.message,
+                     self.name, self.uuid, self.logic))
