eb600b50d7b18dcbb64323d6186d580c79913f7e
markfickman <mfickman@gmail.com>
2020-11-13 15:12:22 -0800

    #76: unit test duplicate flags

--- flagging_project/tests/test_FlaggingAPI.py
+++ flagging_project/tests/test_FlaggingAPI.py
@@ -7,6 +7,7 @@ from integration_tests import MONGO_DOCKER_IMAGE
 from app import _create_flagging_mongo
 import json
 import re
+from random_object_id import generate
 
 # def make_flag_data_pretty(payload):
 #     data = payload.split("\\")
@@ -119,7 +120,7 @@ def test_get_specific_flag_id_valid(client):
 
 
 #create flag, missing flag name
-def create_flag_missing_flag_name_1(client):
+def test_create_flag_missing_flag_name_1(client):
     # delete all flags
     flag_deletion_url = "flag/delete_all_flags"
     response = client.delete(flag_deletion_url)
@@ -131,20 +132,20 @@ def create_flag_missing_flag_name_1(client):
     assert response.status_code == 401
 
 
-def create_flag_missing_flag_name_2(client):
+def test_create_flag_missing_flag_name_2(client):
     # delete all flags
     flag_deletion_url = "flag/delete_all_flags"
     response = client.delete(flag_deletion_url)
     assert response.status_code == 200
 
     # create flag, no name
-    flag_creation_url = "flag/create_flag/XX"
+    flag_creation_url = "flag/create_flag/XX/"
     response = client.post(flag_creation_url)
-    assert response.status_code == 401
+    assert response.status_code >= 400
 
 
 #create flag, valid
-def create_flag_valid(client):
+def test_create_flag_valid(client):
     # delete all flags
     flag_deletion_url = "flag/delete_all_flags"
     response = client.delete(flag_deletion_url)
@@ -160,3 +161,112 @@ def create_flag_valid(client):
     response = client.delete(flag_deletion_url)
     assert response.status_code == 200
 
+
+#duplicate flag, missing flag id
+def test_duplicate_flag_missing_flag_id(client):
+    # delete all flags
+    flag_deletion_url = "flag/delete_all_flags"
+    response = client.delete(flag_deletion_url)
+    assert response.status_code == 200
+
+    # create flag
+    flag_creation_url = "flag/create_flag/XX/Flag1A"
+    response = client.post(flag_creation_url)
+    assert response.status_code == 200
+
+    flag_duplicate_missing_id_url = "flag/duplicate_flag"
+    response = client.post(flag_duplicate_missing_id_url)
+    assert response.status_code == 401
+
+    # delete all flags
+    flag_deletion_url = "flag/delete_all_flags"
+    response = client.delete(flag_deletion_url)
+    assert response.status_code == 200
+
+
+#duplicate flag, flag id does not exist
+def test_duplicate_flag_id_does_not_exist(client):
+# delete all flags
+    flag_deletion_url = "flag/delete_all_flags"
+    response = client.delete(flag_deletion_url)
+    assert response.status_code == 200
+
+    # create flag
+    flag_creation_url = "flag/create_flag/XX/Flag1A"
+    response = client.post(flag_creation_url)
+    assert response.status_code == 200
+
+
+    # get new flag id
+    flag_id_get_url = "flag/get_flag_ids"
+    response = client.get(flag_id_get_url)
+    assert response.status_code == 200
+
+    # unpack flag id
+    x = response.get_data().decode("utf-8")
+    id = re.sub("[^a-zA-Z0-9]+", "", x.split(":")[1])
+    new_id = str(generate())
+    while new_id == id:
+        new_id = str(generate())
+
+    url_duplicate_flag_does_not_exist = "flag/duplicate_flag/" + new_id
+    response = client.post(url_duplicate_flag_does_not_exist)
+    assert response.status_code == 404
+
+    # delete all flags
+    flag_deletion_url = "flag/delete_all_flags"
+    response = client.delete(flag_deletion_url)
+    assert response.status_code == 200
+
+
+#duplicate flag, invalid flag id
+def test_duplicate_flag_invalid_flag_id(client):
+    flag_deletion_url = "flag/delete_all_flags"
+    response = client.delete(flag_deletion_url)
+    assert response.status_code == 200
+
+    # create flag
+    flag_creation_url = "flag/create_flag/XX/Flag1A"
+    response = client.post(flag_creation_url)
+    assert response.status_code == 200
+
+    url_invalid_duplicate_flag = "flag/duplicate_flag/1A"
+    response = client.post(url_invalid_duplicate_flag)
+    assert response.status_code == 406
+
+    # delete all flags
+    flag_deletion_url = "flag/delete_all_flags"
+    response = client.delete(flag_deletion_url)
+    assert response.status_code == 200
+
+
+#dupplicate flag, valid
+def test_duplicate_flag_id_valid(client):
+    # delete all flags
+    flag_deletion_url = "flag/delete_all_flags"
+    response = client.delete(flag_deletion_url)
+    assert response.status_code == 200
+
+    # create flag
+    flag_creation_url = "flag/create_flag/XX/Flag1A"
+    response = client.post(flag_creation_url)
+    assert response.status_code == 200
+
+    #get new flag id
+    flag_id_get_url = "flag/get_flag_ids"
+    response = client.get(flag_id_get_url)
+    assert response.status_code == 200
+
+    # unpack flag id
+    x = response.get_data().decode("utf-8")
+    id = re.sub("[^a-zA-Z0-9]+", "", x.split(":")[1])
+
+
+    url_duplicate_flag_valid = "flag/duplicate_flag/" + id
+    response = client.post(url_duplicate_flag_valid)
+    assert response.status_code == 200
+
+    # delete all flags
+    flag_deletion_url = "flag/delete_all_flags"
+    response = client.delete(flag_deletion_url)
+    assert response.status_code == 200
