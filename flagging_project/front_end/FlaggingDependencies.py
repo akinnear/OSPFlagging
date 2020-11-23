0d1eec6bbc964695ec925cd6b1713c5318779303
markfickman <mfickman@gmail.com>
2020-11-23 11:49:00 -0800

    #76: keep mock method get_dependenceies for ease of unit testing

--- flagging_project/front_end/FlaggingDependencies.py
+++ flagging_project/front_end/FlaggingDependencies.py
@@ -1,33 +1,33 @@
-# from flag_data.FlaggingMongo import FlaggingMongo
-# from front_end.FlaggingSchemaInformation import FlaggingSchemaInformation
-# from flagging.FlaggingValidation import validate_flag_logic_information
-# from flagging.FlagLogicInformation import FlagLogicInformation
-# from flagging.FlaggingNodeVisitor import CodeLocation
-# from flagging.FlagErrorInformation import FlagErrorInformation
-#
-# dependent_flag_column = "DEPENDENT_FLAGS"
-#
-# #A call to get flag dependencies
-# def get_flag_dependencies(*args, **kwargs):
-#     #api endpoint of db endpoint
-#     #hard code dependencies for now
-#     flag_dependencies = {"Flag1": {"Flag8"},
-#      "Flag2": {"Flag3"},
-#      "Flag3": {"Flag4"},
-#      "Flag4": {"Flag5"},
-#      "Flag5": {"Flag6"},
-#      "Flag6": {"Flag7"},
-#      "Flag7": {"Flag8"},
-#      "Flag8": {},
-#      "Flag9": {"Flag10"},
-#      "Flag10": {"Flag9"}}
-#
-#     dummy_flag_deps = kwargs.get("dummy_flag_deps", None)
-#     if dummy_flag_deps:
-#         flag_dependencies = dummy_flag_deps
-#     return flag_dependencies
-#
-# #function to add depedency to flag
+from flag_data.FlaggingMongo import FlaggingMongo
+from front_end.FlaggingSchemaInformation import FlaggingSchemaInformation
+from flagging.FlaggingValidation import validate_flag_logic_information
+from flagging.FlagLogicInformation import FlagLogicInformation
+from flagging.FlaggingNodeVisitor import CodeLocation
+from flagging.FlagErrorInformation import FlagErrorInformation
+
+dependent_flag_column = "DEPENDENT_FLAGS"
+
+#A call to get flag dependencies
+def get_flag_dependencies(*args, **kwargs):
+    #api endpoint of db endpoint
+    #hard code dependencies for now
+    flag_dependencies = {"Flag1": {"Flag8"},
+     "Flag2": {"Flag3"},
+     "Flag3": {"Flag4"},
+     "Flag4": {"Flag5"},
+     "Flag5": {"Flag6"},
+     "Flag6": {"Flag7"},
+     "Flag7": {"Flag8"},
+     "Flag8": {},
+     "Flag9": {"Flag10"},
+     "Flag10": {"Flag9"}}
+
+    dummy_flag_deps = kwargs.get("dummy_flag_deps", None)
+    if dummy_flag_deps:
+        flag_dependencies = dummy_flag_deps
+    return flag_dependencies
+
+#function to add depedency to flag
 # def add_flag_dependencies(flag, new_deps: [], existing_flags: [], all_flag_dependencies, flagging_mongo: FlaggingMongo):
 #     flag_schema_object = None
 #     #remove any possible duplicates from new_deps
