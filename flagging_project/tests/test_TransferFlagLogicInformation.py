aeb4b75850ca9cc699b9f0eac1abe296e575114a
markfickman <mfickman@gmail.com>
2020-11-24 12:28:05 -0800

    #76: test conversion of transfer object JSONiable form back to class object form

--- flagging_project/tests/test_TransferFlagLogicInformation.py
+++ flagging_project/tests/test_TransferFlagLogicInformation.py
@@ -4,7 +4,8 @@ from flagging.VariableInformation import VariableInformation
 from flagging.FlaggingNodeVisitor import CodeLocation
 from flagging.ErrorInformation import ErrorInformation
 from flagging.ModuleInformation import ModuleInformation
-from front_end.TransferFlagLogicInformation import TransferFlagLogicInformation, _convert_FLI_to_TFLI
+from front_end.TransferFlagLogicInformation import TransferFlagLogicInformation, _convert_FLI_to_TFLI, \
+    _convert_TFLI_to_FLI
 
 def test_flag_logic_information_transfer_object_creation():
     #create FLI
@@ -132,6 +133,47 @@ return ff2 < reduce(f, [47,11,42,102,13])"""
     assert transfer_flag_logic_information["errors"][1]["locations"][0]["column_offset"] == 5
     assert transfer_flag_logic_information["errors"][1]["text"] == "y = = =  q2@\n"
     assert transfer_flag_logic_information["errors"][1]["msg"] == "invalid syntax"
+
+def test_flag_logic_information_from_transfer_object_creation():
+    # create FLI
+    flag_logic_information = FlagLogicInformation(
+        used_variables={VariableInformation("ff1"): {CodeLocation(4, 11)},
+                        VariableInformation("ff2"): {CodeLocation(6, 11)},
+                        VariableInformation("a"): {CodeLocation(2, 16), CodeLocation(2, 22)},
+                        VariableInformation("b"): {CodeLocation(2, 26), CodeLocation(2, 34)},
+                        VariableInformation("f"): {CodeLocation(3, 10), CodeLocation(4, 24), CodeLocation(6, 24)}},
+        assigned_variables={VariableInformation("f"): {CodeLocation(2, 0)},
+                            VariableInformation("a"): {CodeLocation(2, 11)},
+                            VariableInformation('b'): {CodeLocation(2, 13)}},
+        referenced_functions={
+            VariableInformation("reduce"): {CodeLocation(3, 3), CodeLocation(4, 17), CodeLocation(6, 17)}},
+        defined_functions={VariableInformation("my_add"): {CodeLocation(2, 4)}},
+        defined_classes={VariableInformation("my_class"): {CodeLocation(1, 1)}},
+        referenced_modules={ModuleInformation("wtforms"): {CodeLocation(2, 5)},
+                            ModuleInformation("functools", "my_funky_tools"): {CodeLocation(1, 7)}},
+        referenced_flags={"Flag5": {CodeLocation(5, 10), CodeLocation(6, 10)}},
+        return_points={CodeLocation(4, 4), CodeLocation(6, 4)},
+        used_lambdas={"LAMBDA": {CodeLocation(2, 4)}},
+        errors=[ErrorInformation(cl=CodeLocation(3, 5),
+                                 msg="invalid syntax",
+                                 text="x = =  f\n"),
+                ErrorInformation(cl=CodeLocation(5, 5),
+                                 msg="invalid syntax",
+                                 text="y = = =  q2@\n")],
+        flag_logic="""
+    f = lambda a,b: a if (a > b) else b
+    if reduce(f, [47,11,42,102,13]) > 100:
+    return ff1 > reduce(f, [47,11,42,102,13])
+    else:
+    return ff2 < reduce(f, [47,11,42,102,13])""",
+        validation_results=TypeValidationResults())
+    # convert to transfer object form
+    transfer_flag_logic_information = _convert_FLI_to_TFLI(flag_logic_information)
+
+    #convert back to flag logic information object
+    fl_object = _convert_TFLI_to_FLI(transfer_flag_logic_information, flag_logic_information)
+
+
 
 
 
