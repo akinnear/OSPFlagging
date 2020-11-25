5c0f3ab03639ebb7cfbba712cb5661e2d617601a
markfickman <mfickman@gmail.com>
2020-11-25 15:35:00 -0800

    #76: simple valid flag convert back to FlagLogicInformation object from transfer object form

--- flagging_project/tests/test_TransferFlagLogicInformation.py
+++ flagging_project/tests/test_TransferFlagLogicInformation.py
@@ -6,6 +6,7 @@ from flagging.ErrorInformation import ErrorInformation
 from flagging.ModuleInformation import ModuleInformation
 from front_end.TransferFlagLogicInformation import TransferFlagLogicInformation, _convert_FLI_to_TFLI, \
     _convert_TFLI_to_FLI
+from flagging.FlaggingNodeVisitor import determine_variables
 
 def test_flag_logic_information_transfer_object_creation():
     #create FLI
@@ -173,7 +174,7 @@ def test_flag_logic_information_from_transfer_object_creation():
     transfer_flag_logic_information = _convert_FLI_to_TFLI(flag_logic_information_og)
 
     #convert back to flag logic information object
-    fl_object = _convert_TFLI_to_FLI(transfer_flag_logic_information, flag_logic_information_og)
+    fl_object = _convert_TFLI_to_FLI(transfer_flag_logic_information)
 
     assert flag_logic_information_og.used_variables == fl_object.used_variables
     assert flag_logic_information_og.assigned_variables == fl_object.assigned_variables
@@ -185,6 +186,35 @@ def test_flag_logic_information_from_transfer_object_creation():
     assert flag_logic_information_og.referenced_flags == fl_object.referenced_flags
     assert flag_logic_information_og.return_points == fl_object.return_points
     assert flag_logic_information_og.used_lambdas == fl_object.used_lambdas
+    assert flag_logic_information_og.errors == fl_object.errors
+
+
+def test_flag_logic_information_from_transfer_object_creation_2():
+    # create FLI
+    logic = """\
+if FF1 > 10:
+    return True
+else:
+    return False"""
+    flag_logic_information_og = determine_variables(logic)
+    # convert to transfer object form
+    transfer_flag_logic_information = _convert_FLI_to_TFLI(flag_logic_information_og)
+
+    #convert back to flag logic information object
+    fl_object = _convert_TFLI_to_FLI(transfer_flag_logic_information)
+
+    assert flag_logic_information_og.used_variables == fl_object.used_variables
+    assert flag_logic_information_og.assigned_variables == fl_object.assigned_variables
+    assert flag_logic_information_og.referenced_functions == fl_object.referenced_functions
+    assert flag_logic_information_og.defined_functions == fl_object.defined_functions
+    assert flag_logic_information_og.defined_classes == fl_object.defined_classes
+    assert flag_logic_information_og.flag_logic == fl_object.flag_logic
+    assert flag_logic_information_og.referenced_modules == fl_object.referenced_modules
+    assert flag_logic_information_og.referenced_flags == fl_object.referenced_flags
+    assert flag_logic_information_og.return_points == fl_object.return_points
+    assert flag_logic_information_og.used_lambdas == fl_object.used_lambdas
+    assert flag_logic_information_og.errors == fl_object.errors
+
 
 
 
