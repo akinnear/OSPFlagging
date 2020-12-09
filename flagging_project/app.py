fda16b74397f62e5a30dfd3b4a679598c1c22d62
markfickman <mfickman@gmail.com>
2020-12-09 09:56:41 -0800

    #76: replace FlaggingMongo with generic FlaggingDOA

--- flagging_project/app.py
+++ flagging_project/app.py
@@ -1,18 +1,18 @@
 import os
 from flask import Flask, jsonify, redirect, request, Response
-from flag_data.FlaggingMongo import FlaggingMongo
+from flag_data.FlaggingDOA import FlaggingDOA
 from handlers.FlaggingAPI import make_routes
 
-def _create_flagging_mongo():
-    return FlaggingMongo("mongodb://localhost:27017/flagging")
+def _create_flagging_doa():
+    return FlaggingDOA("mongodb://localhost:27017/flagging")
 
 app = Flask(__name__)
 # app.register_blueprint(flag_api)
 
 #configure secret key
 app.secret_key = os.urandom(24).hex()
-flagging_mongo = _create_flagging_mongo()
-make_routes(app, flagging_mongo)
+flagging_doa = _create_flagging_doa()
+make_routes(app, flagging_doa)
 
 if __name__ == "__main__":
     app.run(debug=True)
